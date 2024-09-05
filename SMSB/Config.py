d212a8dd44a051d95c020d848ccc78f617a92498
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2024-09-05 19:44:11 +0330

    Update Config.py

--- SMSB/Config.py
+++ SMSB/Config.py
@@ -1,13 +1,1 @@
-# SMS Bomber - Version 5 - ARTorki
-
-expiration = "yyyy-mm-dd"
-password = "PASS"
-contacts = """
- [01] Contact1: .... 9171234567
- [02] Contact2: .... 9121234567
- [03] Contact3: .... 9161234567
- [04] Contact4: .... 9331234567
- [05] Contact5: .... 9361234567
-"""
-telegram_token = "TOKEN"
-telegram_chatid = "CHAT_ID"
+contacts = ["[00] Contact1: .... 9*********", "[01] Contact2: .... 9*********", "[02] Contact3: .... 9*********"]
